9f8a8ed16bbfa89487b3aa48bc054079a585e5e6
Kevin Malby <kevin.malby@gmail.com>
2015-01-16 15:06:35 -0800

    Added empty parser file and removed the internal parser class from frontend.

--- Compilers/ScannerParser/FrontEnd.cs
+++ Compilers/ScannerParser/FrontEnd.cs
@@ -71,21 +71,4 @@ namespace ScannerParser {
        
     }
 
-    public class Parser {
-        private int scannerSym; // current token on input
-
-        public enum Token{
-        ERROR = 0, TIMES = 1, DIV= 2,
-            PLUS = 11, MINUS = 12, EQL = 20, 
-            NEQ, LSS, GEQ, LEQ, GTR,
-            PERIOD = 30, COMMA, OPENBRACKET, CLOSEBRACKET, CLOSEPAREN,
-            BECOMES = 40, THEN, DO, 
-            OPENPAREN = 50, NUMBER = 60, IDENT, 
-            SEMI = 70, END = 80, OD, FI,
-            ELSE = 90, LET = 100, CALL, IF, WHILE, RETURN,
-            VAR = 110, ARR, FUNC, PROC,
-            BEGIN = 150, MAIN = 200, EOF = 255
-};
-        private void Next();
-    }
 }
---
+++ Compilers/ScannerParser/Parser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerParser
+{
+    class Parser
+    {
+        private int scannerSym; // current token on input
+
+        public enum Token
+        {
+            ERROR = 0, TIMES = 1, DIV = 2,
+            PLUS = 11, MINUS = 12, EQL = 20,
+            NEQ, LSS, GEQ, LEQ, GTR,
+            PERIOD = 30, COMMA, OPENBRACKET, CLOSEBRACKET, CLOSEPAREN,
+            BECOMES = 40, THEN, DO,
+            OPENPAREN = 50, NUMBER = 60, IDENT,
+            SEMI = 70, END = 80, OD, FI,
+            ELSE = 90, LET = 100, CALL, IF, WHILE, RETURN,
+            VAR = 110, ARR, FUNC, PROC,
+            BEGIN = 150, MAIN = 200, EOF = 255
+        };
+        private void Next();
+    }
+}
