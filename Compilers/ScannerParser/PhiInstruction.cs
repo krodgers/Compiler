18de4d93e65367977ebbc3d3c93884b084a128c2
Kevin Malby <kevin.malby@gmail.com>
2015-03-11 22:42:25 -0700

    Added phi instruction and the StackList, also made it so that we can keep track of outer joins.

---
+++ Compilers/ScannerParser/PhiInstruction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerParser {
+    public class PhiInstruction : Instruction
+    {
+        public Result originalVarVal;
+        public int symTableID { get; set; }
+        public PhiInstruction(int instructionNumber, BasicBlock myBB, Result originalVarVal) : base(instructionNumber, myBB)
+        {
+            this.originalVarVal = originalVarVal;
+        }
+
+        // Useful for checking whether this is a necessary phi
+        // If the number of operands is only one, the phi
+        // is unnecessary
+        public bool IsValidPhi()
+        {
+            int count = 0;
+            if (firstOperand != null)
+                count++;
+            if (secondOperand != null)
+                count++;
+
+            if (count > 1)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
---
+++ Compilers/ScannerParser/StackList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerParser {
+    class StackList : List<BasicBlock>
+    {
+        public void Push(BasicBlock block)
+        {
+            this.Add(block);
+        }
+
+        public BasicBlock Pop()
+        {
+            BasicBlock returnBlock = this.Last();
+            this.RemoveAt(this.Count - 1);
+            return returnBlock;
+        }
+
+        public BasicBlock Peek()
+        {
+            return this.Last();
+        }
+
+        public BasicBlock GetOuterJoin()
+        {
+            if (this.Count > 1)
+            {
+                var element = this.ElementAt(this.Count - 2);
+                return element;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
